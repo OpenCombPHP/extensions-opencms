2821b0b6ccbb1675bc22bd36c1db0a3f6c2d182c
kongyuan <anubiskong@gmail.com>
2011-12-29 15:44:25 +0800

    修复ckeditor重复初始化的问题

--- class/article/CreateArticle.php
+++ class/article/CreateArticle.php
@@ -51,7 +51,6 @@ class CreateArticle extends ControlPanel
 	
 	public function process()
 	{
-		throw new Exception("fdfsd");
 		//为分类select添加option
 		$aCatSelectWidget = $this->viewArticle->widget("article_cat");
 		
