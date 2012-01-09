ed5521a0e34093341491fda00f055e190ab49e41
kongyuan <anubiskong@gmail.com>
2012-01-09 08:46:31 +0800

    threedot ...

--- public/js/topListShortTitle.js
+++ public/js/topListShortTitle.js
@@ -1,20 +1,9 @@
 jQuery(function(){
 	jQuery('.topList').each(function(b,v){
-		resetTitleLen(v);
-	});
-	jQuery('.topList').resize(function(){
-		resetTitleLen(this);
+		resetTitleLen(jQuery(v));
 	});
 	
 	function resetTitleLen(aTopList){
-		var aTopList = jQuery(aTopList);
-		
-		//如果宽度没有变化就退出
-		if( aTopList.data('oldWidth') && aTopList.width() == aTopList.data('oldWidth')){
-			return;
-		}
-		aTopList.data('oldWidth',aTopList.width());
-		
 		//字符容器的宽度
 		var arrLis = aTopList.find('ul>li');
 		var nLiLen = arrLis.first().width();
@@ -25,7 +14,9 @@ jQuery(function(){
 		
 		//获取所有的链接
 		var arrAs = arrLis.find(">a");
+		
 		arrAs.each(function(b,v){
+			jQuery(v).attr('title',jQuery(v).text());
 			jQuery(v).width(nLiLen - nDataTimeLen - 8);
 		});
 	}
