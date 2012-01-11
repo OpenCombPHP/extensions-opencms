1c86c0e3e335fe70c2a6bc165ef7d463be1304cf
kongyuan <anubiskong@gmail.com>
2012-01-11 17:00:43 +0800

    ..

--- class/frame/CmsFrontController.php
+++ class/frame/CmsFrontController.php
@@ -3,8 +3,6 @@ namespace org\opencomb\opencms\frame ;
 
 use org\opencomb\opencms\frame\CmsFrontFrame;
 use org\opencomb\coresystem\mvc\controller\Controller;
-use org\opencomb\platform\ext\ExtensionManager;
-use org\jecat\framework\mvc\view\Webpage;
 
 class CmsFrontController extends Controller
 {
