ecdad0fd880c6875cdd1d5ddd7f3ee82923e86c0
kongyuan <anubiskong@gmail.com>
2012-06-05 10:30:08 +0800

    更改后台菜单,前台toplist结构

--- bean/widget/control-panel-frame-menu.conf.php
+++ bean/widget/control-panel-frame-menu.conf.php
@@ -1,40 +1,48 @@
 <?php
 return array(
-	'item:CMS' => array(
+	'item:content' => array(
 		'title'=>'内容',
 		'link'=>'?c=org.opencomb.opencms.article.ArticleManage',
 		'query'=>'c=org.opencomb.opencms.article.ArticleManage',
 		'direction'=>'v',
-		
 		'menu'=> 1 ,
-		'item:ArticleManage' => array(
-			'title'=>'文章管理',
-			'link'=>'?c=org.opencomb.opencms.article.ArticleManage',
-			'query'=>array(  //query可以让以下几个页面也带有侧边菜单
-				'c=org.opencomb.opencms.article.ArticleManage' ,
-				'c=org.opencomb.opencms.article.CreateArticle' ,
-				'c=org.opencomb.opencms.article.EditArticle' ,
-			)
-		),
-		'item:CategoryManage' => array(
-			'title'=>'栏目管理',
-			'link'=>'?c=org.opencomb.opencms.category.CategoryManage',
-			'query'=> array(
-				'c=org.opencomb.opencms.category.CategoryManage' ,
-				'c=org.opencomb.opencms.category.CreateCategory' ,
-				'c=org.opencomb.opencms.category.EditCategory' ,
-				'c=org.opencomb.opencms.category.DeleteCategory' ,
-			)
-		),
-		'item:IndexManage' => array(
-			'title'=>'首页设置',
-			'link'=>'?c=org.opencomb.opencms.index.IndexManage',
-			'query'=>'c=org.opencomb.opencms.index.IndexManage'
-		),
-		'item:MenuManage' => array(
-			'title'=>'菜单设置',
-			'link'=>'?c=org.opencomb.opencms.menu.MenuManage',
-			'query'=>'c=org.opencomb.opencms.menu.MenuManage'
+		'item:cms' => array(
+				'title'=>'CMS管理',
+				'link'=>'?c=org.opencomb.opencms.article.ArticleManage',
+				'query'=>array(
+						'c=org.opencomb.opencms.article.ArticleManage' ,
+						'c=org.opencomb.opencms.article.CreateArticle' ,
+						'c=org.opencomb.opencms.article.EditArticle' ,
+				),
+				'item:ArticleManage' => array(
+						'title'=>'文章管理',
+						'link'=>'?c=org.opencomb.opencms.article.ArticleManage',
+						'query'=>array(  //query可以让以下几个页面也带有侧边菜单
+								'c=org.opencomb.opencms.article.ArticleManage' ,
+								'c=org.opencomb.opencms.article.CreateArticle' ,
+								'c=org.opencomb.opencms.article.EditArticle' ,
+						)
+				),
+				'item:CategoryManage' => array(
+						'title'=>'栏目管理',
+						'link'=>'?c=org.opencomb.opencms.category.CategoryManage',
+						'query'=> array(
+								'c=org.opencomb.opencms.category.CategoryManage' ,
+								'c=org.opencomb.opencms.category.CreateCategory' ,
+								'c=org.opencomb.opencms.category.EditCategory' ,
+								'c=org.opencomb.opencms.category.DeleteCategory' ,
+						)
+				),
+				'item:IndexManage' => array(
+						'title'=>'首页设置',
+						'link'=>'?c=org.opencomb.opencms.index.IndexManage',
+						'query'=>'c=org.opencomb.opencms.index.IndexManage'
+				),
+				'item:MenuManage' => array(
+						'title'=>'菜单设置',
+						'link'=>'?c=org.opencomb.opencms.menu.MenuManage',
+						'query'=>'c=org.opencomb.opencms.menu.MenuManage'
+				),
 		),
 	)
 );
